6596bcb7d68c99b4340e2ea43665b36eb30c6bb0
njones430 <87586432+njones430@users.noreply.github.com>
2024-04-24 18:11:44 -0400

    Add files via upload

---
+++ server/server.java
@@ -0,0 +1,179 @@
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.TimeUnit;
+
+public class server {
+
+    static void printBoard(String[] spaces) 
+	{
+		System.out.println("|---|---|---|");
+        System.out.println("| " + spaces[0] + " | " + spaces[1] + " | " + spaces[2] + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + spaces[3] + " | " + spaces[4] + " | " + spaces[5] + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + spaces[6] + " | " + spaces[7] + " | " + spaces[8] + " |");
+        System.out.println("|---|---|---|");
+	}
+
+    static String checkResults(String[] spaces)
+    {
+        boolean isTie = true;
+        for (String s : spaces) 
+        {
+            if (s != "X" && s != "O")
+                isTie = false;
+        }
+        if (isTie)
+            return "Tie";
+
+        String result = "";
+        for (int i = 0; i < 8; i++) 
+        {
+            switch (i) 
+            {
+                case 0:
+                    result = spaces[0] + spaces[1] + spaces[2];
+                    break;
+                case 1:
+                    result = spaces[3] + spaces[4] + spaces[5];
+                    break;
+                case 2:
+                    result = spaces[6] + spaces[7] + spaces[8];
+                    break;
+                case 3:
+                    result = spaces[0] + spaces[3] + spaces[6];
+                    break;
+                case 4:
+                    result = spaces[1] + spaces[4] + spaces[7];
+                    break;
+                case 5:
+                    result = spaces[2] + spaces[5] + spaces[8];
+                    break;
+                case 6:
+                    result = spaces[0] + spaces[4] + spaces[8];
+                    break;
+                case 7:
+                    result = spaces[2] + spaces[4] + spaces[6];
+                    break;
+            }
+            if (result.equals("XXX")) 
+                return "Client";
+            else if (result.equals("OOO"))
+                return "Server";
+            else 
+                return "Continue";
+        }
+        return "";
+    }
+
+    public static void main(String[] args) throws Exception {
+        String command;
+        String modifiedSentence;
+        String[] spaces = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        String result = "";
+        int response = 0;
+
+        // Establish port
+        int port = Integer.parseInt(args[0]);
+
+        // Create socket at port
+        ServerSocket servSocket = new ServerSocket(port);        
+        
+        OutputStream fileOut = null;
+        while(true) {
+            Socket connectionSocket = servSocket.accept();
+            String clientIP = connectionSocket.getRemoteSocketAddress().toString();
+            System.out.println("Client accepted with IP: " + clientIP.substring(1));
+
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+
+            // Create output stream attached to the socket
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+            outToClient.writeBytes("Let's play tic-tac-toe! You are playing with X's.\n");
+            while((command = inFromClient.readLine()) != null) {
+                if("bye".equals(command)) {
+                    outToClient.writeBytes("Disconnected\n");
+                    System.out.println("Disconnected");
+                    //TimeUnit.SECONDS.sleep(1);
+                    break;
+                }
+                /*else if("Joke 1".equals(command)) {
+                    System.out.println("Client requested: " + command + ", returning joke");
+                    outToClient.writeBytes("joke1.txt\n");
+                    TimeUnit.SECONDS.sleep(1);
+    
+                    File file = new File("../jokes/joke1.txt");
+                    long length = file.length();
+                    outToClient.writeLong(length);
+                    byte[] bytes = new byte[8192];
+                    InputStream in = new FileInputStream(file);
+                    OutputStream out = connectionSocket.getOutputStream();
+                    
+                    int count;
+                    while((count = in.read(bytes)) > 0) {
+                        out.write(bytes, 0, count);
+                    }
+
+                    in.close();
+
+                }*/
+                else {
+                    // Desired modifications to sentence
+                    try 
+                    {
+                        response = Integer.parseInt(command);
+                    }
+                    catch (NumberFormatException e) 
+                    {
+                        modifiedSentence = "\"" + command + "\"" + " is an invalid input, try again\n";
+                        outToClient.writeBytes(modifiedSentence);
+                        continue;
+                    }
+                    spaces[response - 1] = "X";
+                    printBoard(spaces);
+                    result = checkResults(spaces);
+                    if (result.equals("Server") || result.equals("Client") || result.equals("Tie"))
+                        break;
+                    System.out.println("Enter a number displayed on the board:");
+                    modifiedSentence = inFromUser.readLine();
+                    try 
+                    {
+                        response = Integer.parseInt(modifiedSentence);
+                    }
+                    catch (NumberFormatException e) 
+                    {
+                        modifiedSentence = "\"" + modifiedSentence + "\"" + " is an invalid input, try again\n";
+                        outToClient.writeBytes(modifiedSentence);
+                        continue;
+                    }
+                    spaces[response - 1] = "O";
+                    outToClient.writeBytes(modifiedSentence);
+                }
+
+            }
+            if (result.equals("Client")) 
+            {
+                outToClient.writeBytes("You win!");
+                System.out.println("You lose...");
+            }
+            else if (result.equals("Server")) 
+            {
+                outToClient.writeBytes("You lose...");
+                System.out.println("You win!!");
+            }
+            else if (result.equals("Tie")) 
+            {
+                outToClient.writeBytes("It's a tie!");
+                System.out.println("It's a tie!");
+            }
+
+            outToClient.close();
+            inFromClient.close();
+
+        }
+
+    }
+};
